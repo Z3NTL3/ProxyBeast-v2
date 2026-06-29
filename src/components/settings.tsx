264eea429439d7debcd78341ad0c8bc364c03558
z3ntl3 <efdal.pix4@outlook.com>
2026-06-29 17:04:35 +0200

    06/29/2026 17:04:35

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,5 +1,12 @@
 import { useContext, useEffect } from "react"
 import { ScreenContext } from "../screen.context"
+import { LuSettings2 } from "react-icons/lu";
+import { Switch } from "@/components/ui/switch"
+import { SiTraefikproxy } from "react-icons/si";
+import { Slider } from "@/components/ui/slider"
+import { Badge } from "@/components/ui/badge"
+import URI_Tooltip from "./uri-tooltip";
+import { MdTimer } from "react-icons/md";
 
 export default function Settings() {
   let screen = useContext(ScreenContext)
@@ -10,6 +17,83 @@ export default function Settings() {
   }, [screen])
 
   return (
-    <h1>settings</h1>
+    <div className="flex flex-col p-5 mx-8 mt-10">
+      <h1 className="text-2xl">Settings</h1>
+      <p className="text-xs text-gray-400">
+        Configure your applications settings.
+      </p>
+
+
+      {/* settings pane */}
+      <div className="grid grid-cols-2 gap-x-10 items-start justify-center mt-14">
+
+        {/* pane item */}
+        <div className="flex flex-col bg-[#2A2A45] w-[350px] h-fit rounded-md p-4 border border-white/20">
+          <div className="flex items-center gap-x-1 mb-2">
+            <LuSettings2 size={22} color="#4D6AF0" />
+            <h2 className="text-[18px]">General</h2>
+          </div>
+
+          {/* sub item */}
+          <div className="flex mt-2">
+            <div className="flex flex-col">
+              <h3 className="text-[14px] text-gray-300">Auto-Retry</h3>
+              <p className="text-[12px] text-gray-400">Retry failed connections automatically.</p>
+            </div>
+            <div className="flex grow  justify-end items-center">
+              <Switch />
+            </div>
+          </div>
+          {/* end */}
+
+          {/* sub item */}
+          <div className="flex mt-2">
+            <div className="flex w-full flex-col">
+              <h3 className="text-[14px] text-gray-300">Pool Size</h3>
+              <p className="text-[12px] text-gray-400">Customize the worker pool size.</p>
+
+              <div className="flex grow w-full justify-end items-end">
+                <Badge className="text-[#0058BC] bg-[#D8E2FF] text-[12px]">
+                  1000
+                </Badge>
+              </div>
+              <Slider className="mt-2" defaultValue={[1000]} max={10_000} step={10} />
+            </div>
+          </div>
+          {/* end */}
+        </div>
+        {/* end */}
+
+        {/* pane item */}
+        <div className="flex flex-col bg-[#2A2A45] w-[350px] h-fit rounded-md p-4 border border-white/20">
+          <div className="flex items-center gap-x-1 mb-2">
+            <SiTraefikproxy size={22} color="#4D6AF0" />
+            <h2 className="text-[18px]">Connection</h2>
+          </div>
+
+          {/* sub item */}
+          <div className="flex mt-2">
+            <div className="flex flex-col w-full">
+              <h3 className="text-[14px] text-gray-300">Timeout</h3>
+              <p className="text-[12px] text-gray-400">Ignore proxies with higher latency than.</p>
+              <div className="flex grow w-full justify-end items-end">
+                <Badge className="text-[#0058BC] bg-[#D8E2FF] text-[12px]">
+                  5000ms
+                </Badge>
+              </div>
+            </div>
+          </div>
+          <Slider className="mt-2" defaultValue={[5]} max={30} step={1} />
+          <p className="mt-5 text-gray-400 text-[12px]">
+            Our proxy checker uses <URI_Tooltip/> schemes to detect multi protocol proxies. Your file must contain proxies in <URI_Tooltip/> format.
+          </p>
+          {/* end */}
+        </div>
+        {/* end */}
+
+
+      </div>
+      {/* end */}
+    </div>
   )
 }
--- src/layout.tsx
+++ src/layout.tsx
@@ -13,86 +13,88 @@ import { invoke } from "@tauri-apps/api/core";
 import { Toaster } from "@/components/ui/sonner"
 import "./App.css";
 import logo from "./assets/logo.png";
-
+import { TooltipProvider } from "@/components/ui/tooltip"
 
 export const Layout = memo(() => {
   let screenData = useContext(ScreenContext)
 
   return (
-    <div
-      className="flex w-screen h-screen bg-[#1E1E2E] overflow-hidden"
-    >
-      <motion.div animate={{opacity: [0, 1]}} layout className="bg-[#2A2A45] w-60 h-full p-5 border-r border-[#808080]/40">
-        {/*logo*/}
-        <div data-tauri-drag-region className="flex items-center">
-          <img width={40} src={logo} />
-          <h2 className="text-[24px] font-bold">ProxyBeast</h2>
-        </div>
-        {/*end*/}
-
-        {/*version*/}
-        <p className="text-xs text-white/40 mt-2 ml-1">v{sessionStorage.getItem("version")}</p>
-        {/*end*/}
-
-        {/*items*/}
-        <div className="flex flex-col gap-y-4 mt-10">
-          {SCREENS.map((screen_, i) => (
-            <React.Fragment key={`screen-${i}`}>
-              <a key={`screen-${i}`} href={screen_.href} onClick={async () => {
-                await invoke("stop_check")
-                return true;
-              }}>
-              {
-                screenData.current === screen_.title ?
-                    <motion.div animate={{scaleY: [0,1]}} className="hover:shadow-[1px_1px_30px_0.1px_rgba(53,120,236,0.4)] bg-[#0A84FF] cursor-pointer flex items-center gap-x-2 px-2 py-1.5 rounded-lg font-inter text-[13px] text-left">
-                      {screen_.node}
-                    </motion.div>
-                  :
-                    <div className="hover:text-white/60 text-white/40 cursor-pointer flex items-center gap-x-2 px-2 py-1.5 rounded-lg font-inter text-[13px] text-left">
-                      {screen_.node}
-                    </div>
-                }
-              </a>
-            </React.Fragment>
-          ))}
-        </div>
-        {/*end*/}
-
-        <div className="mt-100 flex w-full h-[0.2px] bg-white/20"></div>
-        <p className="mt-5 flex items-center justify-center gap-x-1 text-center text-[12px] text-white/40 hover:text-white hover:cursor-pointer">
-          <FaRegQuestionCircle />
-          Support
-        </p>
-      </motion.div>
-      <main className="flex flex-col w-full h-full items-start">
-        <motion.div
-          layout animate={{opacity: [0, 1]}}
-          data-tauri-drag-region
-          className="flex bg-[#2A2A45] w-full h-10 p-3 font-inter text-[13px] text-white/80 border-b border-[#808080]/40"
-        >
-          {screenData.current}
-          <div className="flex grow items-center justify-end gap-x-1">
-            <MdMinimize
-              className="font-bold"
-              fontSize={18}
-              onClick={() => Window.getCurrent().minimize()}
-            />
-            <MdOutlineClose
-              className="font-bold"
-              fontSize={18}
-              onClick={() => Window.getCurrent().close()}
-            />
+    <TooltipProvider>
+      <div
+        className="flex w-screen h-screen bg-[#1E1E2E] overflow-hidden"
+      >
+        <motion.div whileInView={{opacity: [0, 1]}} layout className="bg-[#2A2A45] w-60 h-full p-5 border-r border-[#808080]/40">
+          {/*logo*/}
+          <div data-tauri-drag-region className="flex items-center">
+            <img width={40} src={logo} />
+            <h2 className="text-[24px] font-bold">ProxyBeast</h2>
           </div>
-        </motion.div>
+          {/*end*/}
 
-        <motion.div className="w-full h-full" layout animate={{ opacity: [0, 1] }} transition={{
-          type: "keyframes",
-          ease: "easeIn"
-        }}>
-          <Outlet />
-          <Toaster />
+          {/*version*/}
+          <p className="text-xs text-white/40 mt-2 ml-1">v{sessionStorage.getItem("version")}</p>
+          {/*end*/}
+
+          {/*items*/}
+          <div className="flex flex-col gap-y-4 mt-10">
+            {SCREENS.map((screen_, i) => (
+              <React.Fragment key={`screen-${i}`}>
+                <a key={`screen-${i}`} href={screen_.href} onClick={async () => {
+                  await invoke("stop_check")
+                  return true;
+                }}>
+                {
+                  screenData.current === screen_.title ?
+                      <motion.div whileInView={{scaleY: [0,1]}} className="hover:shadow-[1px_1px_30px_0.1px_rgba(53,120,236,0.4)] bg-[#0A84FF] cursor-pointer flex items-center gap-x-2 px-2 py-1.5 rounded-lg font-inter text-[13px] text-left">
+                        {screen_.node}
+                      </motion.div>
+                    :
+                      <div className="hover:text-white/60 text-white/40 cursor-pointer flex items-center gap-x-2 px-2 py-1.5 rounded-lg font-inter text-[13px] text-left">
+                        {screen_.node}
+                      </div>
+                  }
+                </a>
+              </React.Fragment>
+            ))}
+          </div>
+          {/*end*/}
+
+          <div className="mt-100 flex w-full h-[0.2px] bg-white/20"></div>
+          <p className="mt-5 flex items-center justify-center gap-x-1 text-center text-[12px] text-white/40 hover:text-white hover:cursor-pointer">
+            <FaRegQuestionCircle />
+            Support
+          </p>
         </motion.div>
-      </main>
-    </div>
+        <main className="flex flex-col w-full h-full items-start">
+          <motion.div
+            layout whileInView={{opacity: [0, 1]}}
+            data-tauri-drag-region
+            className="flex bg-[#2A2A45] w-full h-10 p-3 font-inter text-[13px] text-white/80 border-b border-[#808080]/40"
+          >
+            {screenData.current}
+            <div className="flex grow items-center justify-end gap-x-1">
+              <MdMinimize
+                className="font-bold"
+                fontSize={18}
+                onClick={() => Window.getCurrent().minimize()}
+              />
+              <MdOutlineClose
+                className="font-bold"
+                fontSize={18}
+                onClick={() => Window.getCurrent().close()}
+              />
+            </div>
+          </motion.div>
+
+          <motion.div className="w-full h-full" layout whileInView={{ opacity: [0, 1] }} transition={{
+            type: "keyframes",
+            ease: "easeIn"
+          }}>
+            <Outlet />
+            <Toaster />
+          </motion.div>
+        </main>
+      </div>
+    </TooltipProvider>
   );
 })
